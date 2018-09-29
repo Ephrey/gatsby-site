bef2659fbfbf8653b986748606c6699908702d60
Ephraim Ilunga <ephreycleophace@gmail.com>
2018-09-29 10:53:55 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi Everyone !</h1>
+    <p>I am Ephraim Ilunga, a selftought developer who loves code.</p>
+    <p>I am from South-Africa and staying in Durban.</p>
+    <b>Regards !</b>
+  </Layout>
+)
+
+export default IndexPage
